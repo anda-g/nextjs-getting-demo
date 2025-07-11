bdcb08542ad79f15bca15b4a0f8ab5b08b0ab691
KungSovannda <kungsovannda@gmail.com>
2025-07-11 10:38:57 +0700

    update: data table with tanstack and apply some features such as sort search select

--- src/app/(admin)/dashboard/DataTableClient.tsx
+++ src/app/(admin)/dashboard/DataTableClient.tsx
@@ -1,31 +1,185 @@
+// "use client";
+// import Loading from "@/app/loading";
+// import { fetcher } from "@/services/fetcher";
+// import { Product } from "@/types/productType";
+// import { ColumnDef, useReactTable } from "@tanstack/react-table";
+// import useSWR from "swr";
+// // import DataTable, { TableColumn } from "react-data-table-component";
+
+// const columns: ColumnDef<Product>[] = [
+//   {
+//     header: "Id",
+//     accessorKey: "id",
+//   },
+//   {
+//     header: "Title",
+//     accessorKey: "title",
+//   },
+//   {
+//     header: "Price",
+//     accessorKey: "price",
+//   },
+//   {
+//     header: "Stock",
+//     accessorKey: "stock",
+//   },
+//   {
+//     header: "Category",
+//     accessorKey: "category",
+//   },
+// ];
+
+// // export default function DataTableClient() {
+// //   const { data, isLoading } = useSWR<{ products: Product[] }>(
+// //     `${process.env.NEXT_PUBLIC_BASE_API_URL}?limit=100`,
+// //     fetcher
+// //   );
+// //   if (isLoading) return <Loading />;
+// //   return <DataTable pagination columns={columns} data={data?.products || []} />;
+// // }
+
+// export default function DataTableClient() {
+//   const { data, isLoading } = useSWR<{ products: Product[] }>(
+//     `${process.env.NEXT_PUBLIC_BASE_API_URL}?limit=100`,
+//     fetcher
+//   );
+
+//   const table = useReactTable({
+//     data: data?.products ?? [],
+//     columns,
+//   });
+//   if (isLoading) return <Loading />;
+//   return <div>DataTableClient</div>;
+// }
+
 "use client";
 import Loading from "@/app/loading";
 import { fetcher } from "@/services/fetcher";
 import { Product } from "@/types/productType";
-import DataTable, { TableColumn } from "react-data-table-component";
+import { ColumnDef } from "@tanstack/react-table";
 import useSWR from "swr";
 
-const columns: TableColumn<Product>[] = [
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { ArrowUpDown } from "lucide-react";
+import DataTableComponent from "@/components/data-table/DataTableComponent";
+
+const columns: ColumnDef<Product>[] = [
   {
-    name: "Id",
-    selector: (row) => row.id,
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+    enableResizing: false,
+    size: 30,
   },
   {
-    name: "Title",
-    selector: (row) => row.title,
+    header: "ID",
+    accessorKey: "id",
+    enableResizing: false,
+    size: 80,
   },
   {
-    name: "Price",
-    selector: (row) => row.price,
-    sortable: true,
+    header: "Product title",
+    accessorKey: "title",
+    enableResizing: false,
+    size: 400,
   },
   {
-    name: "Stock",
-    selector: (row) => row.stock,
+    header: ({ column }) => {
+      return (
+        <span
+          className="flex items-center cursor-pointer"
+          onClick={() => {
+            column.toggleSorting(column.getIsSorted() === "asc");
+          }}
+        >
+          Price <ArrowUpDown className="ml-2 h-3 w-3" />
+        </span>
+      );
+    },
+    accessorKey: "price",
   },
   {
-    name: "Category",
-    selector: (row) => row.category,
+    header: ({ column }) => {
+      return (
+        <span
+          className="flex items-center cursor-pointer"
+          onClick={() => {
+            column.toggleSorting(column.getIsSorted() === "asc");
+          }}
+        >
+          Stock <ArrowUpDown className="ml-2 h-3 w-3" />
+        </span>
+      );
+    },
+    accessorKey: "stock",
+  },
+  {
+    header: "Category",
+    accessorKey: "category",
+    cell: ({ row }) => {
+      const rawValue = row.getValue("category");
+      const category =
+        typeof rawValue === "string"
+          ? rawValue.replace(/-/g, " ").replace(/\b\w/g, (c) => c.toUpperCase())
+          : "";
+      return <span>{category}</span>;
+    },
+  },
+  {
+    header: "Action",
+    cell: ({ row }) => {
+      const product = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(String(product.id))}
+            >
+              Copy product ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View product</DropdownMenuItem>
+            <DropdownMenuItem className="text-red-600">
+              Delete product
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
 
@@ -34,6 +188,10 @@ export default function DataTableClient() {
     `${process.env.NEXT_PUBLIC_BASE_API_URL}?limit=100`,
     fetcher
   );
-  if (isLoading) return <Loading />;
-  return <DataTable pagination columns={columns} data={data?.products || []} />;
+
+  if (isLoading || !data) return <Loading />;
+
+  console.log(data);
+
+  return <DataTableComponent columns={columns} data={data?.products} />;
 }
---
+++ src/components/data-table/DataTableComponent.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+type DataTableComponentProp<TData> = {
+  columns: ColumnDef<TData>[];
+  data: TData[];
+};
+
+export default function DataTableComponent<TData>({
+  columns,
+  data,
+}: DataTableComponentProp<TData>) {
+  const [sorting, setSorting] = useState<SortingState>([
+    { id: "id", desc: false },
+  ]);
+
+  const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 10 });
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [rowSelection, setRowSelection] = useState({});
+  const table = useReactTable({
+    data: data ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      pagination,
+      columnFilters,
+      rowSelection,
+    },
+    onPaginationChange: setPagination,
+  });
+
+  return (
+    <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Enter product title..."
+          value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("title")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead
+                    key={header.id}
+                    style={{ width: `${header.getSize()}px` }}
+                    className="font-semibold"
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      <div className="flex items-center justify-between  py-4">
+        <div className="text-muted-foreground flex-1 text-sm">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
