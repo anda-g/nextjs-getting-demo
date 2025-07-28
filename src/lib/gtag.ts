7d1f10ba636dff37e36ddad3cc713072e248e644
KungSovannda <kungsovannda@gmail.com>
2025-07-28 11:00:58 +0700

    fix: gtag type

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,4 +1,11 @@
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+
 // Log page views
 export const pageview = (url: string) => {
   if (!GA_TRACKING_ID) return;
@@ -15,13 +22,32 @@ export const event = ({
 }: {
   action: string;
   category: string;
-  label: string;
-  value: number;
+  label?: string;
+  value?: number;
 }) => {
   if (!GA_TRACKING_ID) return;
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
+  });
+};
+
+// Add just a few useful e-commerce events
+export const trackPurchase = (orderId: string, value: number) => {
+  if (!GA_TRACKING_ID) return;
+  window.gtag("event", "purchase", {
+    transaction_id: orderId,
+    value: value,
+    currency: "USD",
+  });
+};
+
+export const trackAddToCart = (itemName: string, value: number) => {
+  if (!GA_TRACKING_ID) return;
+  window.gtag("event", "add_to_cart", {
+    currency: "USD",
+    value: value,
+    item_name: itemName,
   });
 };
